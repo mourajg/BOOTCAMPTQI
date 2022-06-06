001580812949062fb3396723cb706ab9d773bac9
João Gabriel Moura <105926520+mourajg@users.noreply.github.com>
2022-06-06 17:06:53 -0300

    Update DESAFIO1.java

--- DESAFIO1.java
+++ DESAFIO1.java
@@ -1,43 +1,29 @@
-O objetivo deste desafio é resolvê-lo utilizando o seu conhecimento sobre Arrays, uma estrutura de dados que armazena uma coleção de dados em um bloco de memória.
-
-Você está desenvolvendo um sistema para um local de eventos, neste local, é necessário que a entrada seja permitida apenas para pessoas acima de 18 anos. Faça um programa para ler um número N, a idade de N pessoas. Depois disso, coloque a idade mínima permitida  para se entrar no local, conforme exemplo.
-
-
+>>>
+/* O objetivo deste desafio é resolvê-lo utilizando o seu conhecimento sobre Arrays, uma estrutura
+de dados que armazena uma coleção de dados em um bloco de memória.
+Faça um programa para ler um número N (idade de N pessoas). Depois disso, coloque qual idade será
+considerada mínima para se entrar no local.
 Retorne às idades de quem não puder entrar.
-  
-  import java.util.*;
- 
-    public class DIO{
+*/
 
-	//complete o código para que ele funcione corretamente
- 
-        public static void main(String[] args) {
+import java.util.*;
 
-            Scanner scan = new Scanner(System.in);
-
-            int N= scan.nextInt();
-            
-            String string = scan.nextLine();         
-       
-            //String[] s = string.split(" ");     
-          
-
-            double[] idade = new double[N];
-
-            for (int i = 0; i < N; i++)
-            {
-                string = scan.nextLine();                
-                idade[i] = Integer.parseInt(string);    
-            }
-
-            System.out.println("Nao poderao entrar as idades: ");
-
-            for (int i = 0; i < N; i++)
-            {
-                if(idade[i] < 18) {
-                    System.out.println((int) idade[i]);
-                }
-            }
-                   
-        }
-    }
+public class DIO {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int N = scan.nextInt();
+		String string = scan.nextLine();
+		double[] idade = new double[N];
+		for (int i = 0; i < N; i++) {
+			string = scan.nextLine();
+			idade[i] = Integer.parseInt(string);
+		}
+		System.out.println("Não poderão entrar: "); /* Atenção aqui! Coloquei acento para ficar bem redigido, no seu código os acentos deverão sumir. */
+		for (int i = 0; i < N; i++) {
+			if(idade[i] < 18) {
+				System.out.println((int) idade[i]);
+			}
+		}
+	}
+}
+>>>
