e5e4cb26a73412dd5289cd1993ea311dcc41dc81
João Gabriel Moura <105926520+mourajg@users.noreply.github.com>
2022-06-06 15:42:53 -0300

    Update DESAFIO2.java

--- DESAFIO2.java
+++ DESAFIO2.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.util.*;
 
 public class DIO {
-  
 	public static void main(String[] args) throws IOException {
 		while(input.hasNext()){
         		int entrada = input.nextInt();
