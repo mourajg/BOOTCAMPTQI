188fa29401e0de7f722714d7b83cb6565778d640
João Gabriel Moura <105926520+mourajg@users.noreply.github.com>
2022-06-07 07:58:47 -0300

    Create DESAFIO2.java

---
+++ DESAFIO2.java
@@ -0,0 +1,22 @@
+>>>
+/* ENTRADA - Um número inteiro C será informado (quantidade de casos de teste). Cada caso de teste
+inicia com um número inteiro N (1 ≤ N ≤ 1000), indicando o número de termos da soma.
+SAÍDA - Para cada caso de teste imprima um número S, que é o resultado da soma dos N termos da expressão.
+*/
+
+import java.io.IOException;
+import java.util.*;
+public class DIO {
+        public static void main(String[] args) throws IOException {
+                Scanner input = new Scanner(System.in);
+                while(input.hasNext()) {
+                        int entrada = input.nextInt();
+                        if(entrada % 2 == 0) {
+                                System.out.println("0");
+                        } else {
+                                System.out.println("1");
+                        }
+                }
+        }
+}
+>>>
