62fe919766c226d6d6f988f26da5d9e6175649d6
João Gabriel Moura <105926520+mourajg@users.noreply.github.com>
2022-06-06 22:52:20 -0300

    Update DESAFIO4.java

--- DESAFIO4.java
+++ DESAFIO4.java
@@ -10,25 +10,25 @@ escolhas PAR ou ÍMPAR e os números, informe quem foi o vencedor.
 */
 
 import java.util.*;
-
 public class DIO {
-    public static void main(String[] args) {
-        Scanner leitor = new Scanner(System.in);
-        int QT = Integer.parseInt(leitor.nextLine());
-        String linha1[], linha2[], nome1, escolha1, nome2, escolha2;
-        int N, M;
-        for (int i = 0; i < QT; i++) {
-            linha1 = leitor.nextLine().split(" ");
-            linha2 = leitor.nextLine().split(" ");
-            N = Integer.parseInt(linha2[0]);
-            M = Integer.parseInt(linha2[1]);
-            nome1 = linha1[0];
-            escolha1 = linha1[1];
-            nome2 = linha1[2];
-            escolha2 = linha1[3];
-            if (((N + M) % 2 == 0 && escolha1.equalsIgnoreCase("PAR")) || ((N + M) % 2 != 0 && escolha1.equalsIgnoreCase("IMPAR"))) System.out.println(nome1);
-            else System.out.println(nome2);
+        public static void main(String[] args) {
+                Scanner leitor = new Scanner(System.in);
+                int QT = Integer.parseInt(leitor.nextLine());
+                String linha1[], linha2[], nome1, escolha1, nome2, escolha2;
+                int N, M;
+                for (int i = 0; i < QT; i++) {
+                        linha1 = leitor.nextLine().split(" ");
+                        linha2 = leitor.nextLine().split(" ");
+                        N = Integer.parseInt(linha2[0]);
+                        M = Integer.parseInt(linha2[1]);
+                        nome1 = linha1[0];
+                        escolha1 = linha1[1];
+                        nome2 = linha1[2];
+                        escolha2 = linha1[3];
+                        if (((N + M) % 2 == 0 && escolha1.equalsIgnoreCase("PAR")) 
+                        || ((N + M) % 2 != 0 && escolha1.equalsIgnoreCase("IMPAR"))) System.out.println(nome1);
+                        else System.out.println(nome2);
+                }
         }
-    }
 }
 >>>
