e738da5caa8ef5308b7683d063d705a647c07f26
João Gabriel Moura <105926520+mourajg@users.noreply.github.com>
2022-06-07 07:59:40 -0300

    Update DESAFIO2.java

--- DESAFIO2.java
+++ DESAFIO2.java
@@ -1,5 +1,5 @@
 >>>
-/* ENTRADA - Um número inteiro C será informado (quantidade de casos de teste). Cada caso de teste
+/* ENTRADA - Um número inteiro C (quantidade de casos de teste) será informado. Cada caso de teste
 inicia com um número inteiro N (1 ≤ N ≤ 1000), indicando o número de termos da soma.
 SAÍDA - Para cada caso de teste imprima um número S, que é o resultado da soma dos N termos da expressão.
 */
