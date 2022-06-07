adbd5e2cbc37a2b67be6599598e9111a8d8e75fb
João Gabriel Moura <105926520+mourajg@users.noreply.github.com>
2022-06-06 22:43:33 -0300

    Update DESAFIO2.java

--- DESAFIO2.java
+++ DESAFIO2.java
@@ -6,18 +6,17 @@ SAÍDA - Para cada caso de teste imprima um número S, que é o resultado da som
 
 import java.io.IOException;
 import java.util.*;
-
 public class DIO {
-  public static void main(String[] args) throws IOException {
-    Scanner input = new Scanner(System.in);
-    while(input.hasNext()) {
-      int entrada = input.nextInt();
-      if(entrada % 2 == 0) {
-        System.out.println("0");
-      } else {
-        System.out.println("1");
+        public static void main(String[] args) throws IOException {
+                Scanner input = new Scanner(System.in);
+                while(input.hasNext()) {
+                        int entrada = input.nextInt();
+                        if(entrada % 2 == 0) {
+                                System.out.println("0");
+                        } else {
+                                System.out.println("1");
+                        }
+                }
         }
-    }
-  }
 }
 >>>
