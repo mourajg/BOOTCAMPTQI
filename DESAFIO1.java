e1b7a82ec1af42ed91839c9223251e1d76a88ed6
João Gabriel Moura <contatomoura2000@gmail.com>
2022-06-29 17:44:13 -0300

    Update DESAFIO1.java

--- DESAFIO1.java
+++ DESAFIO1.java
@@ -1,25 +1,19 @@
 >>>
-/* O objetivo deste desafio é resolvê-lo utilizando o seu conhecimento sobre Arrays, uma estrutura
-de dados que armazena uma coleção de dados em um bloco de memória. Faça um programa para ler um número
-N (idade de N pessoas). Depois disso, coloque qual idade será considerada mínima para se entrar no 
-local. Retorne às idades de quem não puder entrar.
-*/
-
 import java.util.*;
 public class DIO {
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		int N = scan.nextInt();
-		String string = scan.nextLine();
-		double[] idade = new double[N];
-		for (int i = 0; i < N; i++) {
-			string = scan.nextLine();
-			idade[i] = Integer.parseInt(string);
+	public static void main (String [] args) {
+		Scanner scan = new Scanner (System.in);
+		int N = scan.nextInt ();
+		String string = scan.nextLine ();
+		double [] idade = new double [N];
+		for (int i = 0; i < N; i ++) {
+			string = scan.nextLine ();
+			idade [i] = Integer.parseInt (string);
 		}
-		System.out.println("Não poderão entrar: "); /* Atenção aqui! */
-		for (int i = 0; i < N; i++) {
-			if(idade[i] < 18) {
-				System.out.println((int) idade[i]);
+		System.out.println ("Não poderão entrar: "); // Atenção aqui!
+		for (int i = 0; i < N; i ++) {
+			if (idade [i] < 18) {
+				System.out.println ((int) idade [i]);
 			}
 		}
 	}
