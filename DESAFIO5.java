f408a39869b2ca016b07073a058627c8facc3248
João Gabriel Moura <contatomoura2000@gmail.com>
2022-06-29 18:02:55 -0300

    Update DESAFIO5.java

--- DESAFIO5.java
+++ DESAFIO5.java
@@ -1,30 +1,23 @@
 >>>
-/* Gilberto é um famoso vendedor de esfirras na região. Porém, apesar de todos gostarem de suas
-esfirras, ele só sabe dar o troco com duas notas, ou seja, nem sempre é possível receber o troco
-certo. Para facilitar para ele, escreva um programa para determinar se é possível ou não devolver
-o troco exato utilizando duas notas.
-Notas disponíveis: 2, 5, 10, 20, 50 e 100.
-*/
-
 import java.io.IOException;
 import java.util.Scanner;
 public class DIO {
-	public static void main(String[] args) throws IOException {
-		Scanner leitor = new Scanner(System.in);
+	public static void main (String [] args) throws IOException {
+		Scanner leitor = new Scanner (System.in);
 		while (true) {
-			int N = leitor.nextInt();
-			int M = leitor.nextInt();
+			int N = leitor.nextInt ();
+			int M = leitor.nextInt ();
 			int troco = M - N;
-			int[] notas = {2, 5, 10, 20, 50, 100};
+			int [] notas = {2, 5, 10, 20, 50, 100};
 			boolean possivel = false;
 			if (N == 0 && M == 0) break;
-			for (int i = 0; i < 6; i++) {
-				for (int j = 0; j < 6; j++) {
-					if (troco - notas[j] == notas[i]) possivel = true;
+			for (int i = 0; i < 6; i ++) {
+				for (int j = 0; j < 6; j ++) {
+					if (troco - notas [j] == notas [i]) possivel = true;
 				}
 			}
-			if (possivel) System.out.println("possible");
-			else System.out.println("impossible");
+			if (possivel) System.out.println ("possible");
+			else System.out.println ("impossible");
 		}
 	}
 }
