616840cb49e138171814299944d7166b5d0a57b1
João Gabriel Moura <contatomoura2000@gmail.com>
2022-07-07 20:50:35 -0300

    Update and rename DESAFIO4.java to DESAFIOSDECÓDIGOJAVA/4.java

--- DESAFIOSDECÓDIGOJAVA/4.java
+++ DESAFIOSDECÓDIGOJAVA/4.java
@@ -6,7 +6,7 @@ public class DIO {
 		int QT = Integer.parseInt (leitor.nextLine ());
 		String linha1 [], linha2 [], nome1, escolha1, nome2, escolha2;
 		int N, M;
-		for (int i = 0; i < QT; i ++) {
+		for (int i = 0; i < QT; i++) {
 			linha1 = leitor.nextLine ().split (" ");
 			linha2 = leitor.nextLine ().split (" ");
 			N = Integer.parseInt (linha2 [0]);
@@ -15,8 +15,11 @@ public class DIO {
 			escolha1 = linha1 [1];
 			nome2 = linha1 [2];
 			escolha2 = linha1 [3];
-			if (((N + M) % 2 == 0 && escolha1.equalsIgnoreCase ("PAR")) || ((N + M) % 2 != 0 && escolha1.equalsIgnoreCase ("IMPAR"))) System.out.println (nome1);
-			else System.out.println (nome2);
+			if (((N + M) % 2 == 0 && escolha1.equalsIgnoreCase ("PAR")) || ((N + M) % 2 != 0 && escolha1.equalsIgnoreCase ("IMPAR"))) {
+				System.out.println (nome1);
+			} else {
+				System.out.println (nome2);
+			}
 		}
 	}
 }
